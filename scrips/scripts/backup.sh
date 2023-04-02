594b6730ae705487970d9cc32b76b6b99539fd03
Balázs Erik Andor <erik@sloth.szepezdi>
2023-04-02 14:45:39 +0200

    update

---
+++ scrips/scripts/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#make backup folder to home
+mkdir ~/.backup
+
+date=$(date '+%Y-%m-%d')
+
+mkdir ~/.backup/$date
+cd ~/.backup/$date
+
+
---
+++ scrips/scripts/setup_debian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+sudo apt install git neovim htop powertop docker docker-compose nginx wireguard stow syncthing zsh exa
+
+echo "sudo visudo -> erik       ALL=(ALL:ALL) NOPASSWD:ALL"
