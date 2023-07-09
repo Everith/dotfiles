956adbd9dfd9b5d38c72159361565a5d48c298c4
Everith <balazserik000@bgmail.com>
2023-07-09 15:23:43 +0200

    update restore

--- scrips/scripts/restore.sh
+++ scrips/scripts/restore.sh
@@ -7,26 +7,25 @@ tar zxf /srv/backup/latest.tar.gz -C /srv/backup/temp
 
 
 # Install applications: 
-apt update && apt upgrade -y
-apt install nginx samba git docker docker-compose wireguard tmux transmission-daemon curl wget -y
+sudo apt update && apt upgrade -y
+sudo apt install nginx samba git docker docker-compose wireguard tmux transmission-daemon curl wget exa btop zsh -y
 
 # Cockpit install 
-#
-# apt install cockpit cockpit-storaged cockpit-networkmanager 
-# cd /tmp
-# curl -LO https://github.com/45Drives/cockpit-identities/releases/download/v0.1.12/cockpit-identities_0.1.12-1focal_all.deb
-# apt install ./cockpit-identities_0.1.12-1focal_all.deb
-# rm cockpit-identities_0.1.12-1focal_all.deb
-# cd 
+sudo apt install cockpit cockpit-storaged cockpit-networkmanager 
+sudo cd /tmp
+sudo curl -LO https://github.com/45Drives/cockpit-identities/releases/download/v0.1.12/cockpit-identities_0.1.12-1focal_all.deb
+sudo apt install ./cockpit-identities_0.1.12-1focal_all.deb
+sudo rm cockpit-identities_0.1.12-1focal_all.deb
+sudo cd 
 
 # Install youtube-dl
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-chmod a+rx /usr/local/bin/yt-dlp  # Make executable
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
 
-chown -R erik:erik /srv
+sudo chown -R erik:erik /srv
 
-cp -r $restore_root/etc/* /etc/
-cp -r $restore_root/srv/* /srv/
-cp -r $restore_root/var/* /var/
+sudo cp -r $restore_root/etc/* /etc/
+sudo cp -r $restore_root/srv/* /srv/
+sudo cp -r $restore_root/var/* /var/
 
-rm -r /srv/backup/temp
+sudo rm -r /srv/backup/temp
