87869a24efc7a696fdfc61c4b377877b8c8f6db6
Everith <balazserik000@gmial.com>
2024-05-26 21:18:10 +0200

    update

--- scrips/scripts/arch/install.sh
+++ scrips/scripts/arch/install.sh
@@ -22,13 +22,22 @@ mount "${TARGET_DISK}1" /mnt/boot
 swapon "${TARGET_DISK}2"
 
 # Install base system
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel linux linux-firmware git neovim
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into the new system
 arch-chroot /mnt
+
+sudo pacman -S git neovim btop powertop stow syncthing zsh exa swaybar bitwarden wofi firefox discord thunar thunar-volman gvfs gvfs-smb gvfs-mtp sshfs tumbler feh pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep network-manager-applet playerctl jq polkit-gnome pamixer gnome-control-center thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja zenity socat clang npm unzip gvfs-smb kitty 
+sudo pacman -S xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-headers
+sudo pacman -S ripgrep playerctl
+sudo pacman -S zenity socat xf86-video-intel
+# JAPANESE characters
+sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
+pacman -S sshfs gvfs-mtp thunar-volman
+sudo pacman -S cdrtools cdrdao
 
 echo "Password for root"
 passwd
@@ -48,40 +57,14 @@ pacman -S limine
 limine-install /dev/sda  # Install Limine to the disk
 
 # Create a user
-useradd -m -G wheel erik
+useradd -mG wheel erik
 echo "Password for user"
 passwd erik
 
 # Enable necessary services (e.g., NetworkManager)
 systemctl enable NetworkManager
 
-sudo pacman -S git neovim btop powertop stow syncthing zsh exa swaybar bitwarden wofi firefox discord thunar thunar-volman gvfs gvfs-smb gvfs-mtp sshfs tumbler feh pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol wireplumber ripgrep network-manager-applet playerctl jq polkit-gnome pamixer gnome-control-center thunar-archive-plugin file-roller xdg-user-dirs wf-recorder dbus-python python-gobject python-requests python-jinja zenity socat clang npm unzip gvfs-smb kitty 
-sudo pacman -S xdg-desktop-portal-hyprland xorg-xwayland qt5-wayland qt6-wayland qt5ct qt6ct libva linux-headers
-sudo pacman -S ripgrep playerctl
-sudo pacman -S zenity socat
-# JAPANESE characters
-sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
-
-pacman -S sshfs gvfs-mtp thunar-volman
-sudo pacman -S cdrtools cdrdao
 sudo modprobe cifs
-
-#YAY install
-cd ~/temp/
-git clone https://aur.archlinux.org/yay.git 
-cd yay 
-makepkg -si PKGBUILD
-cd ~/temp/
-rm -r yay
-
-yay -S gradience-git adw-gtk3-git eww-tray-wayland-git swww grimblast-git python-material-color-utilities hyprpicker-git
-
-# Fonts and icons
-yay -S ttf-jetbrains-mono ttf-nerd-fonts-symbols papirus-icon-theme
-#Set up fonst and icons
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
-gsettings set org.gnome.desktop.interface icon-theme Papirus
-gsettings set org.gnome.desktop.interface font-name "JetBrains Mono Regular 11"
 
 # Exit chroot
 exit
