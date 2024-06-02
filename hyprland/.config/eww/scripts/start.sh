5d8512d0ad42750368024ae5f53ac6bbea2ca7a8
Balázs Erik Andor <balazserik000@gmail.com>
2024-06-02 13:06:15 +0200

    back to the beggining ...

--- hyprland/.config/eww/scripts/start.sh
+++ hyprland/.config/eww/scripts/start.sh
@@ -2,6 +2,6 @@
 pkill eww
 eww daemon
 eww open bar
-#eww open bg_widgets
-#eww open notifications_popup
-#~/.config/eww/scripts/notifications.py &
+eww open bg_widgets
+eww open notifications_popup
+~/.config/eww/scripts/notifications.py &
