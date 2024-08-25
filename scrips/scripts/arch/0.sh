8c92df3d80302506b7eb73d79f18e59b9bd865ec
Everith <balazserik000@gmail.com>
2024-08-25 16:34:03 +0200

    update

--- scrips/scripts/arch/0.sh
+++ scrips/scripts/arch/0.sh
@@ -17,12 +17,7 @@ mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 iso=$(curl -4 ifconfig.co/country-iso) #ez lényegében iso=HU
 reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
-# Prompt the user for input
-read -p "Automated (a) or manual (b)? " choice
-
-case "$choice" in
-    a|A)
-      sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
       o # clear the in memory partition table
       n # new partition
       p # primary partition
@@ -52,42 +47,47 @@ EOF
       mount /dev/sda2 /mnt
       mkdir /mnt/boot
       mount /dev/sda1 /mnt/boot
+
+# Prompt the user for input
+read -p "Automated (a) or manual (b)? " choice
+case "$choice" in
+    a|A)
       ;;
 
     b|B)
-      lsblk
-      echo "Enter the drive: (eg.: /dev/sda )"
-      read DRIVE
-      sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${DRIVE}
-      o # clear the in memory partition table
-      n # new partition
-      p # primary partition
-      1 # partition number 1
-        # default - start at beginning of disk 
-      +1G # 1GB boot parttion
-      n # new partition
-      p # primary partition
-      2 # partion number 2
-        # default, start immediately after preceding partition
-        # default, extend partition to end of disk
-      t # change type of partition
-      2 # select partition number 2
-      8e # select LVM type
-      p # print the in-memory partition table
-      w # write the partition table
-      q # and we're done
-EOF
-      # Format the partitions
-      mkfs.fat -F32 ${DRIVE}1
-      mkfs.ext4 -L root ${DRIVE}2
+      # lsblk
+      # echo "Enter the drive: (eg.: /dev/sda )"
+      # read DRIVE
+      # sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${DRIVE}
+      # o # clear the in memory partition table
+      # n # new partition
+      # p # primary partition
+      # 1 # partition number 1
+      #   # default - start at beginning of disk 
+      # +1G # 1GB boot parttion
+      # n # new partition
+      # p # primary partition
+      # 2 # partion number 2
+      #   # default, start immediately after preceding partition
+      #   # default, extend partition to end of disk
+      # t # change type of partition
+      # 2 # select partition number 2
+      # 8e # select LVM type
+      # p # print the in-memory partition table
+      # w # write the partition table
+      # q # and we're done
+# EOF
+      # # Format the partitions
+      # mkfs.fat -F32 ${DRIVE}1
+      # mkfs.ext4 -L root ${DRIVE}2
 
-      # Format the partitions
-      mkfs.fat -F32 ${DRIVE}1
-      mkfs.ext4 -L root ${DRIVE}2
+      # # Format the partitions
+      # mkfs.fat -F32 ${DRIVE}1
+      # mkfs.ext4 -L root ${DRIVE}2
 
-      mount ${DRIVE}2 /mnt
-      mkdir /mnt/boot
-      mount ${DRIVE}1 /mnt/boot
+      # mount ${DRIVE}2 /mnt
+      # mkdir /mnt/boot
+      # mount ${DRIVE}1 /mnt/boot
       ;;
 
     c|C)
