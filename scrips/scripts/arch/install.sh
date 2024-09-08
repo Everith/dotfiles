0a33c77c90e970d5e68de43c65ac9f6322e3a368
Everith <balazserik000@gmail.com>
2024-09-08 14:52:04 +0200

    update

--- scrips/scripts/arch/install.sh
+++ scrips/scripts/arch/install.sh
@@ -3,9 +3,14 @@
 source env.conf
 
 bash prepareenviroment.sh
+cp servercreds /mnt/root/servercreds
+cp -r .ssh /mnt/root/
+
 arch-chroot /mnt bash /root/rootsettings.sh
 arch-chroot /mnt /usr/bin/runuser -u $USER -- bash /home/$USER/usersettings.sh
 arch-chroot /mnt bash /root/rootfinish.sh
+
+
 
 echo "#############################"
 echo "# Installation is completed #"
